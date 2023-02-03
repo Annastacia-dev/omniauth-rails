bd95a57cf05131bb50d123989f231675ec9b9c5e
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2023-02-03 14:44:39 +0300

    change database to postgresql

--- Gemfile
+++ Gemfile
@@ -10,7 +10,12 @@ gem "rails", "~> 7.0.4"
 gem "sprockets-rails"
 
 # Use sqlite3 as the database for Active Record
-gem "sqlite3", "~> 1.4"
+# gem "sqlite3", "~> 1.4"
+
+# Use postgesql as the database for Active Record
+gem "pg"
+
+# Use Puma as the app server
 
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", "~> 5.0"
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_02_03_064958) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "username"
