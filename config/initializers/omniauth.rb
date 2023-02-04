1534f75ed43b23efa4e025f15d67a179317de552
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2023-02-04 18:38:44 +0300

    facebook auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], redirect_uri: "https://railsomniauth.onrender.com/auth/facebook/callback", client_id: ENV['FACEBOOK_APP_ID'], state: "facebook"
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
   provider :twitter2, ENV["TWITTER_CLIENT_ID"], ENV["TWITTER_CLIENT_SECRET"], callback_path: '/auth/twitter2/callback', scope: "tweet.read users.read", headers:{
     Authorization: "Basic #{Base64.strict_encode64("#{ENV["TWITTER_CLIENT_ID"]}:#{ENV["TWITTER_CLIENT_SECRET"]}")}"
